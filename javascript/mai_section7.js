b7a0ba3f58c49f014722abb81dbe0eb2979a32ee
Nang0102 <ntmainbm6328@gmail.com>
2022-02-09 15:10:51 +0700

    update footer

--- javascript/mai_section7.js
+++ javascript/mai_section7.js
@@ -1,7 +1,24 @@
-let slideIndex = 1;
-khoiSlide(slideIndex);
+// let slideIndex = 1;
+// khoiSlide(slideIndex);
 
-function plusSlides(n) {
-   (slideIndex += n);
+// function plusSlides(n) {
+//    (slideIndex += n);
+//   }
+
+  var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
   }
-
+  
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+  }

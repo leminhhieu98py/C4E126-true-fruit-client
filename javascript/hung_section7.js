e6714ea3baa444a148b3befc35d8817aa83a0746
Hung <hungelegant@gmail.com>
2022-02-12 22:08:31 +0700

    api section 7

--- javascript/hung_section7.js
+++ javascript/hung_section7.js
@@ -1,28 +1,27 @@
-// let slideIndex = 1;
-// khoiSlide(slideIndex);
-
-// function plusSlides(n) {
-//    (slideIndex += n);
-//   }
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   var i;
-//   var x = document.getElementsByClassName("slide");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length} ;
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   x[slideIndex-1].style.display = "block";
-// }
-
+function getSection7Data() {
+  const big_div_section7 = document.getElementById("section7_big_container");
+  fetch("https://6207b20522c9e90017d32ead.mockapi.io/section7")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        big_div_section7.insertAdjacentHTML(
+          "beforeend",
+          `<div class="silde">
+        <p>
+        ${data[i][paragraph]}
+        </p>
+        <p class="people">
+         ${data[i][author]}
+          <br />
+          ${data[i][description]}  
+        </p>
+      </div>`
+        );
+      }
+    });
+}
+getSection7Data();
 
 var slideIndex = 1;
 showSlides(slideIndex);

d4d5fd103958aa9f0624df26f770b6dc7bee621b
Hieu. Le Minh - CMC Global DU1.16 <lmhieu1@cmcglobal.vn>
2022-02-15 14:07:25 +0700

    backup api

--- javascript/thanh_section4.js
+++ javascript/thanh_section4.js
@@ -1,7 +1,7 @@
 function getSection4Data() {
   const big_div_section4 = document.getElementById("section4_big_container");
 
-  fetch("https://5f749eab1cf3c900161cd568.mockapi.io/api/section4")
+  fetch("https://truejuice.free.beeceptor.com/section4")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
@@ -52,3 +52,30 @@ function getSection4Data() {
 }
 
 getSection4Data();
+
+// [
+//   {
+//     "img_url": "https://truejuice.vn/images/home/usp-1.jpg",
+//     "head_title": "Liệu trình chuyên biệt",
+//     "paragraphs": [
+//       "Thiết kế bởi 'Natural Juice Therapist' (NTJ) - Ms. Trần Thanh Huyền là người đầu tiên tại Việt Nam nghiên cứu đạt chứng nhận về liệu pháp cải thiện sức khỏe với nước ép tự nhiên - liệu pháp được sử dụng bởi các chuyên gia dinh dưỡng hàng đầu trên thế giới.",
+//       "Mỗi liệu trình được thiết kế và sử dụng các đặc tính chữa bệnh có trong các loại nước ép từ rau xanh, củ quả và các siêu thực phẩm(superfood) cung cấp enzymes, vitamins, khoáng chất, acid amin, chất chống oxy hóa giúp cơ thể chữa lành và có sức khỏe tối ưu."
+//     ]
+//     },
+//     {
+//       "img_url": "https://truejuice.vn/images/home/usp-2.jpg",
+//       "head_title": "Tiêu chuẩn sức khỏe",
+//       "paragraphs": [
+//         "100% nguyên liệu rau xanh canh tác hữu cơ rõ ràng nguồn gốc. Luôn tươi ép theo mỗi liệu trình cải thiện sức khỏe từng khách hàng.",
+//         "Nguyên chất. Không pha loãng, không sử dụng đường tinh luyện, chất bảo quản và phụ gia. Chai thủy tinh, bao bì đạt tiêu chuẩn, bao bì thân thiện môi trường. Không sử dụng túi nylon, chai nhựa."
+//       ]
+//       },
+//       {
+//         "img_url": "https://truejuice.vn/images/home/usp-3.jpg",
+//         "head_title": "Công nghệ và dịch vụ hàng đầu",
+//         "paragraphs": [
+//           "Nước ép True Juice sử dụng công nghệ ép lạnh thủy lực hàng đầu tại Việt Nam. Quy trình sản xuất đảm bảo HACCP an toàn và chất lượng.",
+//           "Nước ép True Juice sử dụng công nghệ ép lạnh thủy lực hàng đầu tại Việt Nam. Quy trình sản xuất đảm bảo HACCP an toàn và chất lượng."
+//         ]
+//         }
+// ]
